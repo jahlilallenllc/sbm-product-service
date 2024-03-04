315cf625585f102f6480d7a9479555ec316c6805
xxxjahlilallenxxx <jahlilallen@gmail.com>
2024-03-04 16:21:33 -0500

    Updated ProductController

--- src/main/java/org/jallenllc/productservice/controller/ProductController.java
+++ src/main/java/org/jallenllc/productservice/controller/ProductController.java
@@ -1,11 +1,15 @@
 package org.jallenllc.productservice.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.jallenllc.productservice.dto.ProductRequest;
+import org.jallenllc.productservice.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,11 +18,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
+@RequiredArgsConstructor
 public class ProductController {
+    private final ProductService productService;
+    
     @PostMapping("/api/products")
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest) {
-        return new String();
+        productService.createProduct(productRequest);
     }
     
 }
